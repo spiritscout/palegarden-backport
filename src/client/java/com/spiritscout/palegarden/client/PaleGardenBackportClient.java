8311045c228fe6cbc84c7a7ba98614ee224d402a
spiritscout <spdx5651@leeds.ac.uk>
2026-05-27 00:31:21 +0100

    Initial commit from Fabric Template

---
+++ src/client/java/com/spiritscout/palegarden/client/PaleGardenBackportClient.java
@@ -0,0 +1,10 @@
+package com.spiritscout.palegarden.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaleGardenBackportClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/spiritscout/palegarden/client/PaleGardenBackportDataGenerator.java
@@ -0,0 +1,11 @@
+package com.spiritscout.palegarden.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PaleGardenBackportDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
