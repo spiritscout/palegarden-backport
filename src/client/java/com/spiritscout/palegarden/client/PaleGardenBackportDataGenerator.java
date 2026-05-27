a76ec9b14b71366aa12c5e1468cdd3dbb6719375
Emily <spdx5651@leeds.ac.uk>
2026-05-27 20:14:32 +0100

    added lootTableProvider for pale planks

--- src/client/java/com/spiritscout/palegarden/client/PaleGardenBackportDataGenerator.java
+++ src/client/java/com/spiritscout/palegarden/client/PaleGardenBackportDataGenerator.java
@@ -1,6 +1,7 @@
 package com.spiritscout.palegarden.client;
 
 import com.spiritscout.palegarden.client.datagen.PaleGardenLanguageProvider;
+import com.spiritscout.palegarden.client.datagen.PaleGardenLootTableProvider;
 import com.spiritscout.palegarden.client.datagen.PaleGardenModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -11,5 +12,6 @@ public class PaleGardenBackportDataGenerator implements DataGeneratorEntrypoint 
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(PaleGardenModelProvider::new);
 		pack.addProvider(PaleGardenLanguageProvider::new);
+		pack.addProvider(PaleGardenLootTableProvider::new);
 	}
 }
---
+++ src/client/java/com/spiritscout/palegarden/client/datagen/PaleGardenLootTableProvider.java
@@ -0,0 +1,20 @@
+package com.spiritscout.palegarden.client.datagen;
+
+import com.spiritscout.palegarden.registry.PaleGardenBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class PaleGardenLootTableProvider extends FabricBlockLootTableProvider {
+
+    public PaleGardenLootTableProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(output, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(PaleGardenBlocks.PALE_OAK_PLANKS);
+    }
+}
