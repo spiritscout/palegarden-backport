1653811326e24fcc4b4293736c67eb9d3e46279c
Emily <spdx5651@leeds.ac.uk>
2026-05-27 19:08:24 +0100

    Add lang provider for planks

--- src/client/java/com/spiritscout/palegarden/client/PaleGardenBackportDataGenerator.java
+++ src/client/java/com/spiritscout/palegarden/client/PaleGardenBackportDataGenerator.java
@@ -1,5 +1,6 @@
 package com.spiritscout.palegarden.client;
 
+import com.spiritscout.palegarden.client.datagen.PaleGardenLanguageProvider;
 import com.spiritscout.palegarden.client.datagen.PaleGardenModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -9,5 +10,6 @@ public class PaleGardenBackportDataGenerator implements DataGeneratorEntrypoint 
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(PaleGardenModelProvider::new);
+		pack.addProvider(PaleGardenLanguageProvider::new);
 	}
 }
---
+++ src/client/java/com/spiritscout/palegarden/client/datagen/PaleGardenLanguageProvider.java
@@ -0,0 +1,20 @@
+package com.spiritscout.palegarden.client.datagen;
+
+import com.spiritscout.palegarden.registry.PaleGardenBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class PaleGardenLanguageProvider extends FabricLanguageProvider {
+
+    public PaleGardenLanguageProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(output, "en_us", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(RegistryWrapper.WrapperLookup wrapperLookup, TranslationBuilder translationBuilder) {
+        translationBuilder.add(PaleGardenBlocks.PALE_OAK_PLANKS, "Pale Oak Planks");
+    }
+}
